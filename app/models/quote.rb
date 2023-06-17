ff7265157c595e15b417c94be0dfe9091fe3722d
Usman Javed <usmanjaved.185203@gmail.com>
2023-06-17 22:06:54 +0500

    implement broadcasting in rails

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quotes: Quote.ordered } }
+  after_update_commit -> { broadcast_replace_to "quotes", partial: "quotes/quote", locals: { quotes: Quote.ordered } }
+  after_destroy_commit -> { broadcast_remove_to "quotes", partial: "quotes/quote", locals: { quotes: Quote.ordered } }
 end
